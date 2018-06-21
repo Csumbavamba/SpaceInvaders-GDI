34f35734e619abe197b94fc7325d4a0bcace3e20
Thomas Sheppard <tomshep65@gmail.com>
2018-06-21 15:54:11 +1200

    Player Lives Implemented

--- Breakout/level.cpp
+++ Breakout/level.cpp
@@ -302,6 +302,7 @@ Level::Draw()
 
     DrawScore();
 	DrawFPS();
+	DrawLives();
 }
 
 void
@@ -416,7 +417,7 @@ Level::Process(float _fDeltaTick)
     CheckForWin();
 	
 
-    
+	UpdatesLives();
 	
    
     
@@ -626,6 +627,7 @@ void Level::CheckAlienBulletCollisions()
 				delete alienBullet;
 				alienBullet = nullptr;
 				hitPoints--;
+				UpdatesLives();
 				PlaySound(MAKEINTRESOURCE(IDR_WAVE_PLAYERHIT), 0, SND_RESOURCE | SND_ASYNC);
 				// Check if Game is Lost
 				if (IsPlayerDead())
@@ -661,13 +663,17 @@ void Level::CheckShipBulletMotherShipCollisions()
 			//Hit the front side of the brick...
 			// bullet->SetY((motherShipY + motherShipHeight / 2.0f) + bulletRadius);
 			/* m_pBall->SetVelocityY(m_pBall->GetVelocityY() * -1);*/
+			
 			motherShip->SetHit(true);
 
 			// TODO make a better respawn for bullet
 			canShoot = true;
 			motherShipCanSpawn = true;
 
+			spaceInvadersScore = spaceInvadersScore + 1000;
 			DestroyMotherShip();
+			UpdateScoreText();
+			
 
 		}
 	}
@@ -716,6 +722,16 @@ void Level::SetBarriersRemaining(int _i)
 	barriersRemaining - _i;
 }
 
+void Level::SetPlayerLives(int _i)
+{
+	hitPoints = _i;
+}
+
+int Level::GetPlayerLives()
+{
+	return hitPoints;
+}
+
 // Mothership functions
 void Level::SpawnMotherShip()
 {
@@ -759,6 +775,17 @@ Level::DrawScore()
     TextOutA(hdc, kiX, kiY, m_strScore.c_str(), static_cast<int>(m_strScore.size()));
 }
 
+void Level::DrawLives()
+{
+	HDC hdc = Game::GetInstance().GetBackBuffer()->GetBFDC();
+
+	const int lifeX = 450;
+	const int lifeY = height - 14;
+	SetBkMode(hdc, OPAQUE);
+
+	TextOutA(hdc, lifeX, lifeY, playerlives.c_str(), static_cast<int>(playerlives.size()));
+}
+
 
 
 void 
@@ -767,6 +794,13 @@ Level::UpdateScoreText()
     m_strScore = "Score: ";
 
     m_strScore += ToString(GetSpaceInvaderScore());
+}
+
+void Level::UpdatesLives()
+{
+	playerlives = "Ship HitPoints: ";
+
+	playerlives += ToString(GetPlayerLives());
 }
 
 
