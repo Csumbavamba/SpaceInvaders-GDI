c740855119bdf98f8433ac335af0fb5e9b36523d
Varun Naval <varun.naval@gmail.com>
2018-06-21 16:26:50 +1200

    Fixed level.cpp header comment

--- Breakout/level.cpp
+++ Breakout/level.cpp
@@ -6,10 +6,10 @@
 //
 // (c) 2018 Media Design School.
 //
-// File Name	: 
-// Description	: 
-// Author		: Your Name
-// Mail			: your.name@mediadesign.school.nz
+// File Name	: level.cpp
+// Description	: Contains code for the level framework of the game where all the entities work on
+// Author		: Thomas Sheppard | Varun Naval | Zsombor Pirok
+// Mail			: thomas.she7816@mediadesignschool.com | varun.nav7759@mediadesignschool.com | zsombor.pir7707@mediadesignschool.com
 //
 
 // Library Includes
